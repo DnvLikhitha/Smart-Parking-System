ca580188f5a99e3909abbfac3a4724c17d6a714c
KommanaboyinaPraveenKumar <142647821+KommanaboyinaPraveenKumar@users.noreply.github.com>
2024-11-23 21:49:26 +0530

    Update change_spot.cpp

--- FinalCodes/change_spot.cpp
+++ FinalCodes/change_spot.cpp
@@ -10,23 +10,23 @@ class Spot_change {
     void update_spot(){
         ofstream file("spots.txt",  ios::trunc);
         int id,type,locality;
-            cout<<"Enter number of sports for 2-wheeler parking at mall: ";
+            cout<<"Enter number of spots for 2-wheeler parking at mall: ";
             cin>>id;
             for(int i=1;i<=id;i++)
             file << i<< " " << "small" << " " << "Mall" << endl;
-            cout<<"Enter number of sports for Car parking at mall: ";
+            cout<<"Enter number of spots for Car parking at mall: ";
             cin>>id;
             for(int i=1;i<=id;i++)
             file << i<< " " << "compact" << " " << "Mall" << endl;
-            cout<<"Enter number of sports for Bus/Truck parking at mall: ";
+            cout<<"Enter number of spots for Bus/Truck parking at mall: ";
             cin>>id;
             for(int i=1;i<=id;i++)
             file << i<< " " << "large" << " " << "Mall" << endl;
-            cout<<"Enter number of sports for Electric parking at mall: ";
+            cout<<"Enter number of spots for Electric parking at mall: ";
             cin>>id;
             for(int i=1;i<=id;i++)
             file << i<< " " << "electric" << " " << "Mall" << endl;
-            cout<<"Enter number of sports for Disabled parking at mall: ";
+            cout<<"Enter number of spots for Disabled parking at mall: ";
             cin>>id;
             for(int i=1;i<=id;i++)
             file << i<< " " << "disabled" << " " << "Mall" << endl;
@@ -34,23 +34,23 @@ class Spot_change {
 
 
 
-            cout<<"Enter number of sports for 2-wheeler parking at Park: ";
+            cout<<"Enter number of spots for 2-wheeler parking at Park: ";
             cin>>id;
             for(int i=1;i<=id;i++)
             file << i<< " " << "small" << " " << "Park" << endl;
-            cout<<"Enter number of sports for Car parking at Park: ";
+            cout<<"Enter number of spots for Car parking at Park: ";
             cin>>id;
             for(int i=1;i<=id;i++)
             file << i<< " " << "compact" << " " << "Park" << endl;
-            cout<<"Enter number of sports for Bus/Truck parking at Park: ";
+            cout<<"Enter number of spots for Bus/Truck parking at Park: ";
             cin>>id;
             for(int i=1;i<=id;i++)
             file << i<< " " << "large" << " " << "Park" << endl;
-            cout<<"Enter number of sports for Electric parking at Park: ";
+            cout<<"Enter number of spots for Electric parking at Park: ";
             cin>>id;
             for(int i=1;i<=id;i++)
             file << i<< " " << "electric" << " " << "Park" << endl;
-            cout<<"Enter number of sports for Disabled parking at Park: ";
+            cout<<"Enter number of spots for Disabled parking at Park: ";
             cin>>id;
             for(int i=1;i<=id;i++)
             file << i<< " " << "disabled" << " " << "Park" << endl;
@@ -58,23 +58,23 @@ class Spot_change {
 
 
 
-            cout<<"Enter number of sports for 2-wheeler parking at Temple: ";
+            cout<<"Enter number of spots for 2-wheeler parking at Temple: ";
             cin>>id;
             for(int i=1;i<=id;i++)
             file << i<< " " << "small" << " " << "Temple" << endl;
-            cout<<"Enter number of sports for Car parking at Park: ";
+            cout<<"Enter number of spots for Car parking at Park: ";
             cin>>id;
             for(int i=1;i<=id;i++)
             file << i<< " " << "compact" << " " << "Temple" << endl;
-            cout<<"Enter number of sports for Bus/Truck parking at Temple: ";
+            cout<<"Enter number of spots for Bus/Truck parking at Temple: ";
             cin>>id;
             for(int i=1;i<=id;i++)
             file << i<< " " << "large" << " " << "Temple" << endl;
-            cout<<"Enter number of sports for Electric parking at Temple: ";
+            cout<<"Enter number of spots for Electric parking at Temple: ";
             cin>>id;
             for(int i=1;i<=id;i++)
             file << i<< " " << "electric" << " " << "Temple" << endl;
-            cout<<"Enter number of sports for Disabled parking at Temple: ";
+            cout<<"Enter number of spots for Disabled parking at Temple: ";
             cin>>id;
             for(int i=1;i<=id;i++)
             file << i<< " " << "disabled" << " " << "Temple" << endl;
